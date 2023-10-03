73e040db5b42194ec1add9545258af78ba60eeac
homeless <vlad.sarkulov@gmail.com>
2023-10-03 22:31:34 +0300

    Initial Commit

---
+++ RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by @hmlssgd on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
